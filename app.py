a6784e1ac626bd7c9d36a3cddb337b0acc9a1fb7
chulhan11jang <chulhan11jang@gmail.com>
2026-07-06 13:03:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -450,7 +450,7 @@ CEO 및 리더들과 함께 걸으며<br>
 """, unsafe_allow_html=True)
 
     st.image(
-        "https://raw.githubusercontent.com/chulhan11jang/CEO_Talk_Plus/main/route.png",
+        "https://raw.githubusercontent.com/chulhan11jang/CEO_Talk_Plus/main/route2.png",
         use_container_width=True
     )
 
